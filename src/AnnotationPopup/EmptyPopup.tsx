a93a07935b687e7298bdf7dad26c5d228b907042
Rainer Simon <rainer@rainersimon.io>
2023-06-12 09:36:53 +0200

    Added delete button

--- src/AnnotationPopup/EmptyPopup.tsx
+++ src/AnnotationPopup/EmptyPopup.tsx
@@ -1,3 +1,4 @@
+import { Trash } from '@phosphor-icons/react';
 import { ReplyForm } from './ReplyForm';
 import { EditableTagList } from './TagList';
 
@@ -7,9 +8,15 @@ import './EmptyPopup.css';
 export const EmptyPopup = () => {
 
   return (
-    <div className="annotation-popup empty">
-      <EditableTagList />
-      <ReplyForm placeholder="Comment..." />
+    <div className="container">
+      <div className="annotation-popup empty">
+        <EditableTagList />
+        <ReplyForm placeholder="Comment..." />
+      </div>
+
+      <button className="anno-desktop-overlay delete">
+        <Trash size={18} />
+      </button>
     </div>
   )
 
